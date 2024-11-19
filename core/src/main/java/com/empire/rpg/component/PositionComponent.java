11458480d56a5031cd581ae019b60b0712f24db0
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2024-11-19 16:33:00 +0100

    .

--- core/src/main/java/com/empire/rpg/component/PositionComponent.java
+++ core/src/main/java/com/empire/rpg/component/PositionComponent.java
@@ -1,97 +1,52 @@
 package com.empire.rpg.component;
 
-/**
- * Représente un composant de position dans le jeu.
- */
+import com.badlogic.gdx.math.Vector2;
+
 public class PositionComponent implements Component {
     private float x;
     private float y;
 
-    /**
-     * Constructeur pour initialiser la position.
-     *
-     * @param x La coordonnée x initiale.
-     * @param y La coordonnée y initiale.
-     */
     public PositionComponent(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Obtient la coordonnée y.
-     *
-     * @return La coordonnée y.
-     */
     public float getY() {
         return y;
     }
 
-    /**
-     * Obtient la coordonnée x.
-     *
-     * @return La coordonnée x.
-     */
     public float getX() {
         return x;
     }
 
-    /**
-     * Définit une nouvelle position.
-     *
-     * @param x La nouvelle coordonnée x.
-     * @param y La nouvelle coordonnée y.
-     */
     public void setPosition(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Affiche la position actuelle.
-     */
-    public void getPosition() {
-        System.out.println("X: " + x + " Y: " + y);
+    // Nouvelle méthode getPosition qui retourne un objet Vector2
+    public Vector2 getPosition() {
+        return new Vector2(x, y);
     }
 
-    /**
-     * Déplace la position actuelle par les valeurs spécifiées.
-     *
-     * @param x La valeur de déplacement en x.
-     * @param y La valeur de déplacement en y.
-     */
     public void move(float x, float y) {
         this.x += x;
         this.y += y;
     }
 
-    /**
-     * Obtient la réduction des dégâts.
-     *
-     * @return Toujours 0 pour ce composant.
-     */
     @Override
     public int getDamageReduction() {
         return 0;
     }
 
-    /**
-     * Définit les points de vie actuels.
-     *
-     * @param i Les points de vie actuels.
-     */
     @Override
     public void setCurrentHealthPoints(int i) {
         // Implémentation vide
     }
 
-    /**
-     * Obtient les points de vie actuels.
-     *
-     * @return Toujours 0 pour ce composant.
-     */
     @Override
     public int getCurrentHealthPoints() {
         return 0;
     }
 }
+
